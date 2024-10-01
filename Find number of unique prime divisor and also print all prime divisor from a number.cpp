08492e5cf2b83f11946aa23d402c6f518fa815f6
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2024-10-01 20:40:22 +0600

    Add files via upload

---
+++ Find number of unique prime divisor and also print all prime divisor from a number.cpp
@@ -0,0 +1,76 @@
+
+// IN THE NAME OF SUPREME AND MERCIFUL GOD
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+const int mx = 1e7 + 10;
+int cnt = 0;
+bitset<mx> isPrime;
+vector<int> prime;
+void sieve(int n)
+{
+
+    for (int i = 3; i <= n; i += 2)
+        isPrime[i] = 1;
+    int sq = sqrt(n);
+    for (int i = 3; i <= sq; i += 2)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                isPrime[j] = 0;
+            }
+        }
+    }
+    isPrime[2] = 1;
+    prime.push_back(2);
+    for(int i=3;i<=n;i+=2){
+        if(isPrime[i]){
+            prime.push_back(i);
+        }
+    }
+}
+vector<int> factorize(int n)
+{
+    vector<int> factor;
+    for (auto p : prime)
+    {
+        if (1LL * p * p > n)
+            break;
+        if (n % p == 0)
+        {
+            cnt++;
+            while (n % p == 0)
+            {
+                factor.push_back(p);
+                n = n / p;
+            }
+        }
+    }
+    if (n > 1)
+    {
+        factor.push_back(n);
+        cnt++;
+    }
+    return factor;
+}
+int main()
+{
+    optimize();
+    int lim = 1e7 + 10;
+    sieve(lim);
+    int t;
+    cin >> t;
+    vector<int> factorization = factorize(t);
+    cout << cnt << endl;
+    for (auto r : factorization)
+    {
+        cout << r << " ";
+    }
+    return 0;
+}
