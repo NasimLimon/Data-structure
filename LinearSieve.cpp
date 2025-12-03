1e5cda0ce8cb851311652ddb3da6191ed84dda8b
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-12-04 00:48:33 +0600

    Update LinearSieve.cpp

--- LinearSieve.cpp
+++ LinearSieve.cpp
@@ -9,7 +9,6 @@
 
 
 // IN THE NAME OF SUPREME & MERCIFUL GOD
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,26 +16,28 @@ using namespace std;
 #define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 const int mx = 1e7 + 5;
-int lp[mx];              // lowest prime (সবচেয়ে ছোট মৌলিক গুণনীয়ক)
-vector<int> primes;      
+
+int lp[mx];     // lowest prime factor
+int hp[mx];     // highest prime factor
+vector<int> primes;
 
 void linear_sieve(int n)
 {
     for (int i = 2; i <= n; i++)
     {
-        if (lp[i] == 0)      // i এখনও composite হিসেবে মার্ক হয়নি
+        if (lp[i] == 0)
         {
-            lp[i] = i;       // i নিজের lowest prime divisor
+            lp[i] = hp[i] = i; // i is prime
             primes.push_back(i);
         }
 
-        // মৌলিক সংখ্যা দিয়ে i এর পরবর্তী সংখ্যাগুলোকে composite marked করা
         for (int p : primes)
         {
-            if (p > lp[i] || p * i > n)
+            if (p > lp[i] || 1LL * p * i > n)
                 break;
 
-            lp[p * i] = p;   // p*i এর সবচেয়ে ছোট prime হলো p
+            lp[p * i] = p;
+            hp[p * i] = max(hp[i], p);
         }
     }
 }
@@ -45,12 +46,13 @@ int main()
 {
     optimize();
 
-    int N = 1e7;
+    int N = 100;
     linear_sieve(N);
 
-    for (int i = 0; i < 50; i++)
-        cout << primes[i] << " ";
+    for (int i = 2; i <= 30; i++)
+    {
+        cout << "Number: " << i 
+             << " | lp: " << lp[i] 
+             << " | hp: " << hp[i] << endl;
+    }
 }
-
-
-
